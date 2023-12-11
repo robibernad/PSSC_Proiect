c327fcfc7b5e0b67cfe97af5d0a927a22de4f700
robibernad <43523984+robibernad@users.noreply.github.com>
2023-12-11 23:06:33 +0200

    Add Client
    Add Client

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadf2c9b4487803a4a4decff40aa9f9638025e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Domain/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Client
+    {
+        public Client(string nume, string prenume, long numarDeTelefon, string adresa)
+        {
+            this.clientId = Guid.NewGuid();
+            this.nume = nume;
+            this.prenume = prenume;
+            this.numarDeTelefon = numarDeTelefon;
+            this.adresa = adresa;
+        }
+        public Guid clientId { get; }
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public long numarDeTelefon { get; set; }
+        public string adresa { get; set; }
+    }
+}
